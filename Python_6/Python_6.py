932f377dbda7f38de2fd0533751b638844de2d51
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-02 11:34:09 +0300

    Ex_5 Task done

--- Python_6/Python_6.py
+++ Python_6/Python_6.py
@@ -1,1 +1,9 @@
 
+input_str = input("Введіть рядок: ")
+
+search_word = input("Введіть слово для пошуку: ")
+replace_word = input("Введіть слово яке хочете замінити: ")
+
+output_str = input_str.replace(search_word, replace_word)
+
+print("Результат: ", output_str)
