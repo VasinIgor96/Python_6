7fb3f63a7e48000ff34e59915356c50bda456124
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-02 10:35:42 +0300

    Ex_2 Task done

--- Python_6/Python_6.py
+++ Python_6/Python_6.py
@@ -1,1 +1,14 @@
 
+string = input("Введіть рядок з словами та цифрами: ")
+
+letter_count = 0
+digit_count = 0
+
+for char in string:
+    if char.isalpha():
+        letter_count += 1
+    elif char.isdigit():
+        digit_count += 1
+
+print("Кількість літер у рядку: ", letter_count)
+print("Кількість цифр у рядку: ", digit_count)
